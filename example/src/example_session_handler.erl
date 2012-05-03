d6de421521afc710bf192042d41921427848a8ee
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-05-02 23:06:28 -0700

    Updates the example

--- example/src/example_session_handler.erl
+++ example/src/example_session_handler.erl
@@ -1,13 +1,13 @@
 -module(example_session_handler).
 -extends(cowboy_session_default_handler).
--export([validate/1, init/1, handle/3]).
+-export([validate/1, init/2, handle/3]).
 
 -record(my_state, { field }).
 
 validate(_Session) ->
    ?MODULE:generate().
 
-init(_Session) ->
+init(_Session, _SessionName) ->
     #my_state{ field = <<"data">> }.
 
 handle(return_field, _Session, #my_state{ field = Field } = State) ->
